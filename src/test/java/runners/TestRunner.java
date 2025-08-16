ca8568bebf25ccbc3f0336bf94606068ddc168fe
ashraf ali <ashrafali2590@gmail.com>
2025-08-16 23:56:42 +0530

    Add runner file

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -14,6 +14,7 @@ import org.testng.annotations.DataProvider;
         "junit:target/cucumber-reports/xml-report/cucumber.xml"
     },
     tags = "@smoke",
+    //tags = "@smoke",
     monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
